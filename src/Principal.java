561bc38622f59d29e38442764bbfb6cd67bf1133
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-10-03 19:47:23 -0300

    ajustes no log

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,17 @@
+import lombok.extern.log4j.Log4j2;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
 public class Principal {
+
+    public static void main(String[] args) {
+
+        List<String> stringsNomesDaMinhaFamilia = Arrays.asList("Carlos", "Tatiana", "Julia", "Laura");
+
+        stringsNomesDaMinhaFamilia.stream().forEach(s -> System.out.println(s));
+
+    }
+
 }
